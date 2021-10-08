37fe8d356b7714f1e4acac62af9f882e431fb6ab
THom1331 <90909474+THom1331@users.noreply.github.com>
2021-10-08 09:10:51 +0200

    Update projetC.c

--- projetC.c
+++ projetC.c
@@ -1,1 +1,57 @@
-hello there !!!
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+
+{
+    string filmCategorieEnfants [3] = {"myckey", "la reine des neiges", "cars"};
+    string filmCategorieAdos [3] = {" Naruto" , "chair de poule", "jumanji"};
+    string filmCategorieAdultes [3] ={ "The Shining", "La Nonne" , "Conjuring"};
+
+   int age = get_int ("quel est ton âge ?");
+
+   if (age<11)
+   {
+       printf("Films Catégorie Enfants\n");
+      for(int i = 0; i < 3 ; i++)
+      {
+       printf ("%s\n",filmCategorieEnfants[i] );
+       string voirfilm = get_string ("Veux-tu voir ce film ?");
+       if (strcmp(voirfilm, "oui") == 0)
+       {
+           return 0;
+       }
+      }
+   }
+   if (age>11 && age<17)
+   {
+       printf("Films Catégorie Ados\n");
+        for(int i = 0; i < 3 ; i++)
+      {
+       printf ("%s\n",filmCategorieAdos[i] );
+        string voirfilm = get_string ("Veux-tu voir ce film ?");
+       if (strcmp(voirfilm, "oui") == 0)
+       {
+           return 0;
+       }
+      }
+   }
+   if (age >18)
+    {
+        printf("Films Catégorie Adultes\n");
+         for(int i = 0; i < 3 ; i++)
+       {
+       printf ("%s\n",filmCategorieAdultes[i]);
+        string voirfilm = get_string ("Veux-tu voir ce film ?");
+       if (strcmp(voirfilm, "oui") == 0)
+       {
+           return 0;
+       }
+      }
+    }
+
+
+
+
+}
