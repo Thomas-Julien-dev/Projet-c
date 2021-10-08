578746333c48093d4f78bd346f8e1e9af4b80918
THom1331 <90909474+THom1331@users.noreply.github.com>
2021-10-08 11:24:05 +0200

    Update projetC.c

--- projetC.c
+++ projetC.c
@@ -4,54 +4,49 @@
 
 int main(void)
 
-{
+{    //je créer 3 tableaux contenant chacun 3 éléments
     string filmCategorieEnfants [3] = {"myckey", "la reine des neiges", "cars"};
     string filmCategorieAdos [3] = {" Naruto" , "chair de poule", "jumanji"};
     string filmCategorieAdultes [3] ={ "The Shining", "La Nonne" , "Conjuring"};
 
    int age = get_int ("quel est ton âge ?");
 
-   if (age<11)
+   if (age<11)//conditions
    {
        printf("Films Catégorie Enfants\n");
-      for(int i = 0; i < 3 ; i++)
+      for(int i = 0; i < 3 ; i++)// boucle 
       {
        printf ("%s\n",filmCategorieEnfants[i] );
        string voirfilm = get_string ("Veux-tu voir ce film ?");
        if (strcmp(voirfilm, "oui") == 0)
        {
-           return 0;
+           return 0;// Retour à l'état initial
        }
       }
    }
-   if (age>11 && age<17)
+   if (age>11 && age<17)//conditions
    {
        printf("Films Catégorie Ados\n");
-        for(int i = 0; i < 3 ; i++)
+        for(int i = 0; i < 3 ; i++)//boucle
       {
        printf ("%s\n",filmCategorieAdos[i] );
         string voirfilm = get_string ("Veux-tu voir ce film ?");
        if (strcmp(voirfilm, "oui") == 0)
        {
-           return 0;
+           return 0;// Retour à l'état initial
        }
       }
    }
-   if (age >18)
+   if (age >18)//conditions
     {
         printf("Films Catégorie Adultes\n");
-         for(int i = 0; i < 3 ; i++)
+         for(int i = 0; i < 3 ; i++)//boucle
        {
        printf ("%s\n",filmCategorieAdultes[i]);
         string voirfilm = get_string ("Veux-tu voir ce film ?");
        if (strcmp(voirfilm, "oui") == 0)
        {
-           return 0;
+           return 0;// Retour à l'état initial
        }
       }
     }
-
-
-
-
-}
